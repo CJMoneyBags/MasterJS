8a78707411be6413bc28511208f478627ffd5152
Chris Bagwell <cjbagwell1@gmail.com>
2024-01-30 16:56:32 -0700

    update

--- Chapter 6/script.js
+++ Chapter 6/script.js
@@ -1,1 +1,49 @@
 // DOM
+// const para = document.querySelector('body > h1');
+// console.log(para);
+
+// const paras = document.querySelectorAll('p');
+
+// const errors = document.querySelectorAll('.error');
+// console.log(errors);
+
+// get an element by ID
+// const title = document.getElementById("page-title");
+// console.log(title);
+
+// // get elements by their class name
+// const errors = document.getElementsByClassName("error");
+// console.log(errors);
+// console.log(errors[0]);
+
+
+// get elements by their tag name
+
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+
+const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText += 'ninjas are awesome!';
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' New Text';
+// });
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+
+// content.innerHTML += '<h2>This is a new h2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});

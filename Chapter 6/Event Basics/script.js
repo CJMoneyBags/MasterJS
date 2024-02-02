ed1356685880b790fbf920eb0689fd8a60033518
Chris Bagwell <cjbagwell1@gmail.com>
2024-02-02 13:18:46 -0700

    updates

--- Chapter 6/Event Basics/script.js
+++ Chapter 6/Event Basics/script.js
@@ -4,13 +4,37 @@
 //     console.log('you clicked me');
 // })
 
+const ul = document.querySelector('ul');
+// ul.remove();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () =>{
+    // ul.innerHTML += '<li>Something new </li>';
+    const li = document.createElement('li');
+    li.textContent = "Something New to do";
+    // ul.append(li);
+    ul.prepend(li);
+})
+
 const items = document.querySelectorAll('li');
 
-items.forEach(li => {
-    li.addEventListener('click', e =>{
-        // console.log('item clicked');
-        // console.log(e.target);
-        // console.log(li);
-        e.target.style.textDecoration = 'line-through';
-    });
+// items.forEach(li => {
+//     li.addEventListener('click', e =>{
+//         // console.log('item clicked');
+//         // console.log(e.target);
+//         // console.log(li);
+//         // e.target.style.textDecoration = 'line-through';
+//     console.log('event in LI');
+//         e.stopPropagation();
+//         e.target.remove();
+//     });
+// });
+
+ul.addEventListener('click' , e =>{
+    // console.log('event in UL');
+    // console.log(e);
+    if(e.target.nodeName === 'LI'){
+    e.target.remove();
+    }
 });

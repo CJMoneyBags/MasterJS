37f67f246a53d485f6e6a2a785c100d035c1f094
Chris Bagwell <cjbagwell1@gmail.com>
2024-02-03 11:57:49 -0700

    update

---
+++ Chapter7/RegEx/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.signup-form');
+// const username = document.querySelector('#username');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    // console.log(username.value);
+    // console.log(form.User.value); // uses Name: 'User'
+    // console.log(form.username.value); /// uses id: 'username'
+
+});
---
+++ Chapter7/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('.signup-form');
+// const username = document.querySelector('#username');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    //console.log(username.value);
+    console.log(form.username.value);
+
+});
+
+
+// testing RegEx
+const username = '1233shaunp';
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+
+// if(result){
+//     console.log('regex test pase :)');
+
+// }
+// else{
+//     console.log('regex test failed :(');
+// }
+
+let result = username.search(pattern);
+
+console.log(result);

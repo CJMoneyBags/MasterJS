abdf743224a6e8eb60d1f5c7a72329d4c9503c72
Chris Bagwell <cjbagwell1@gmail.com>
2024-02-04 10:12:08 -0700

    update

--- Chapter9/Sort Method/script.js
+++ Chapter9/Sort Method/script.js
@@ -1,6 +1,50 @@
-const scores = [10, 5, 0, 90, 60, 10, 20, 70];
+// example 1 - sorting strings
+const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
 
-const firstHighScore = scores.find(score => score > 50);
+// names.sort();
+// names.reverse();
+// console.log(names);
 
+//example 2 - sorting numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
 
-console.log(firstHighScore);
+// scores.sort();
+// scores.reverse();
+// console.log(scores);
+ 
+scores.sort((a,b) => a-b);
+console.log(scores);
+
+// example 3 - sorting objects
+const players = [
+    {name: 'mario', score: 20},
+    {name: 'luigi', score: 10},
+    {name: 'chun-li', score: 50},
+    {name: 'yoshi', score: 30},
+    {name: 'shaun', score: 70}
+
+];
+
+// const scoreOrder = players.sort((a,b) =>{
+//     if(a.score > b.score){
+//         return -1
+//     }else if(b.score > a.score){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// });
+
+const nameOrder = players.sort((a,b) => {
+    if(a.name > b.name){
+        return 1;
+    }else if(b.name > a.name){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}); 
+
+// console.log(scoreOrder);
+console.log(nameOrder);

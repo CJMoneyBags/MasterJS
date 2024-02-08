c0c96cc1abd86eedb70d982477b2e56d411fcc90
Chris Bagwell <cjbagwell1@gmail.com>
2024-02-08 16:53:19 -0700

    update

--- Chapter10 Todo list Project/app.js
+++ Chapter10 Todo list Project/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
     const html = `
@@ -21,4 +22,30 @@ addForm.addEventListener('submit', e =>{
         addForm.reset();
     }
 
+});
+
+// delete todos
+list.addEventListener('click', e =>{
+
+if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+}
+
+});
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter((todo) => todo.textContent.toLocaleLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'));
+    
+};
+
+// keyup event
+search.addEventListener('keyup', () =>{
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
